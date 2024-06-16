8a62d98cef432edc303892b2dd3d26309c56db0a
iskonplus <58971909+iskonplus@users.noreply.github.com>
2024-06-16 20:31:54 +0200

    init: add reactive form to search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+
+  searchProductsForm = new FormGroup({
+    formDataFind: new FormControl(''),
+    formDataCity: new FormControl(''),
+  });
+
+
+  ngOnInit(): void {}
+
+  submit(event: Event) {
+    event.preventDefault();
+    console.log(this.searchProductsForm.value);
+    this.searchProductsForm.reset();
+  }
+
+
 
 }
