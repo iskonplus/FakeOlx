84022b2c00a54f142e9085d9583d1f8e66a5b007
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-04-17 18:09:12 +0200

    feat: add redirect to main page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
+
 export class NavComponent {
+
+  constructor(private router: Router) { }
+
   imgLogo = {
     url: '../../../assets/logo.png',
     alt : 'olx logo',
+  }
+
+  openMainPage() {
+    this.router.navigate(['/']);
   }
 
 }
