d9630f9e33ad180237d266d057442bc31193f283
Artur Tamashevich <arturtamashevich@MacBook-Air-Artur.local>
2025-04-24 16:11:27 +0200

    feat: add user log out

--- src/app/pages/user-page/user-page.component.ts
+++ src/app/pages/user-page/user-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../login-page/services/login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-page',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './user-page.component.scss'
 })
 export class UserPageComponent {
+  constructor(private loginService: LoginService, private router: Router ) {}
+
+  activeUser$ = this.loginService.activeUser$;
+
+  logOut() {
+    this.loginService.clearUser();
+    this.router.navigate(['/']);
+
+  }
 
 }
