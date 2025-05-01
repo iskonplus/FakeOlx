41abfd9e467e690c24dd7c29f0b94d87e89c7184
Artur Tamashevich <arturtamashevich@MacBook-Air-Artur.local>
2025-05-01 22:05:46 +0200

    feat: implement reactive favorite functionality with BehaviorSubject and localStorage

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../../types/product';
+import { CardService } from './card.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-card',
@@ -14,18 +16,31 @@ export class CardComponent {
 
   isCardLike: Boolean = false;
   typeFavoriteIcon = 'favorite_border';
+  private favoriteSub!: Subscription;
 
-  constructor() { }
+  constructor(private cardService: CardService) { }
+
+  ngOnInit(): void {
+    this.favoriteSub = this.cardService.favorites$.subscribe(favIds => {
+      this.isCardLike = favIds.includes(this.product.id);
+      this.typeFavoriteIcon = this.isCardLike ? 'favorite' : 'favorite_border';
+    });
+  }
 
   addToFavorite(event: Event) {
     event.stopPropagation();
-    this.isCardLike = !this.isCardLike;
+    this.cardService.toggleFavorite(this.product.id);
+    this.isCardLike = this.cardService.isFavorite(this.product.id);
     this.typeFavoriteIcon = this.isCardLike ? 'favorite' : 'favorite_border';
   }
 
 
   showMoreDetails() {
     this.showDetails.emit(this.product);
+  }
+
+  ngOnDestroy(): void {
+    this.favoriteSub?.unsubscribe();
   }
 
 }
