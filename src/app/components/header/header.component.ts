38e200b7f05e3a79d6163a63aba827f120e9d13d
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-06-18 19:56:23 +0200

    feat: display count of user's pharmacy ads in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,3 +1,4 @@
+import { ProductsService } from './../../services/products.service';
 import { Component, inject, OnDestroy, ViewChild, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '../../pages/login-page/services/login.service';
@@ -6,6 +7,7 @@ import { PopupComponent } from '../popup/popup.component';
 import { Subscription } from 'rxjs';
 import { UserState } from '../../types/user-state.model';
 import { CardService } from '../../shared/card/card.service';
+import { ValueChangeEvent } from '@angular/forms';
 
 
 @Component({
@@ -18,24 +20,29 @@ export class NavComponent implements OnInit, OnDestroy {
 
   activeUser$ = this.loginService.activeUser$;
   readonly dialog = inject(MatDialog);
-  private userSubscription!: Subscription;
-  favoritesSubscription!: Subscription;
+  private userSubscription?: Subscription;
+  favoritesSubscription?: Subscription;
+  userCartSubscription?: Subscription;
+  userAdsSubscription?: Subscription;
   userState?: UserState;
   totalFavorites = 0;
   totalUserCart? = 0;
+  totalOwnAds = 0;
 
 
 
   @ViewChild('popUp') popupComponent!: PopupComponent;
 
 
-  constructor(private router: Router, private loginService: LoginService, private cardService: CardService) { }
+  constructor(private router: Router, private loginService: LoginService, private cardService: CardService, private productsService: ProductsService) { }
 
   ngOnInit(): void {
     this.userSubscription = this.activeUser$.subscribe(state => this.userState = state);
     this.favoritesSubscription = this.cardService.favorites$.subscribe(favorites => this.totalFavorites = favorites.length);
-    this.cardService.userCart$.subscribe(userCart=> this.totalUserCart = userCart?.totalProductsId.length)
-
+    this.userCartSubscription = this.cardService.userCart$.subscribe(userCart => this.totalUserCart = userCart?.totalProductsId.length)
+    this.userAdsSubscription = this.productsService.userAds$.subscribe(userAds => {
+      this.totalOwnAds = userAds.totalAds.length;
+    });
   }
 
   imgLogo = {
@@ -60,10 +67,10 @@ export class NavComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.favoritesSubscription.unsubscribe();
-    if (this.userSubscription) {
-      this.userSubscription.unsubscribe();
-    }
+    this.favoritesSubscription?.unsubscribe();
+    this.userSubscription?.unsubscribe();
+    this.userCartSubscription?.unsubscribe();
+    this.userAdsSubscription?.unsubscribe();
   }
 
 }
